f43604407e4e19da2b0c058b9ee30874ec3e7e87
Yorick Groeneveld <yorickgroeneveld@hotmail.com>
2020-06-11 11:27:54 +0200

    Apifilter for Organization in Cemetery

--- api/src/Entity/Cemetery.php
+++ api/src/Entity/Cemetery.php
@@ -38,6 +38,7 @@ use Symfony\Component\Validator\Constraints\DateTime;
  * @ApiFilter(OrderFilter::class)
  * @ApiFilter(DateFilter::class, strategy=DateFilter::EXCLUDE_NULL)
  * @ApiFilter(SearchFilter::class, properties={"id":"exact"})
+ * @ApiFilter(SearchFilter::class, properties={"organization":"exact"})
  */
 class Cemetery
 {
